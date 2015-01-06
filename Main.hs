3d0de1582cfaf2f76090c0b8b0151e6e99dd7b09
Alexander Vershilov <alexander.vershilov@gmail.com>
2015-01-06 23:40:47 +0300

    Issue#4: Add options support

--- Main.hs
+++ Main.hs
@@ -12,12 +12,28 @@ import LCB.Utils
 import LCB.Generate
 import Data.TrieMap
 
+import Options
+import Control.Applicative
 import Control.Monad (forM_)
 import Text.PrettyPrint.Leijen.Text
 import qualified Data.Text.Lazy.IO as Text
 
-main = do
-  Right x <- parseFile "1.conf"
+
+data MainOptions = MainOptions
+  { moInput  :: String
+  , moPrefix :: String
+  -- , moTests  :: Bool
+  }
+
+instance Options MainOptions where
+  defineOptions = pure MainOptions
+    <*> simpleOption "input" "" "input file"
+    <*> simpleOption "prefix" "" "prefix in functions and files"
+  --  <*> simpleOption "tests"  True "generate tests"
+
+main = runCommand $ \opts args -> do
+  let finput = moInput opts
+  Right x <- parseFile finput
   let y = buildTrie x
   let tests = fullKeys y
   let (y', alphabet)  = recode y
@@ -29,6 +45,6 @@ main = do
   rnds <- mapM [1..128] $ \i -> do
     ls <- replicateM i (randomRIO (0,255))
   -}
-  let xs = generateFiles y''' r alphabet values tests
+  let xs = generateFiles {-(moPrefix opts)-} y''' r alphabet values tests
   forM_ xs $ \(f,p) ->
      Text.writeFile f (displayT (renderPretty 0.6 80 p))
