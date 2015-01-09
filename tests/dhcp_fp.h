0ab7f3ab663b2aafd89325944a8282bcacb06eac
Dmitry Zuikov <dzuikov@gmail.com>
2015-01-09 15:30:57 +0300

    header file's template added

---
+++ tests/dhcp_fp.h
@@ -0,0 +1,11 @@
+#ifndef __dhcp_fp_h
+#define __dhcp_fp_h
+
+struct dhcp_fp {
+    uint8_t     class_id;
+    const char *class_name;
+    uint16_t    dev_id;
+    const char *dev_name;
+};
+
+#endif
